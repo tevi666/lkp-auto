0800de046c6512fca8a495fe097214ffea0f0280
Təvəkkül <tevizeynalov@gmail.com>
2022-05-27 04:32:37 +0400

    created about__modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -9,21 +9,48 @@ export const modal = () => {
         const btn = document.querySelector('.modal-services');
         const closeBtn = document.querySelector('.close__btn');
 
+        const modalAbout = document.querySelector('.about__modal');
+        const btnAbout = document.querySelector('.about-btn');
+        const closeBtnAbout = document.querySelector('.about__close');
+
         const closeModal = () => {
             modal.style.display = 'none';
         };
         const openModal = () => {
             modal.style.display = 'block';
         };
+
+        const close = () => {
+            modalAbout.style.display = 'none';
+        };
+        const open = () => {
+            modalAbout.style.display = 'block';
+        };
+
         btn.addEventListener('click', e => {
             e.preventDefault();
             e.stopPropagation();
             openModal();
         });
+
         modal.addEventListener('click', e => {
             e.stopPropagation();
         });
+
+        btnAbout.addEventListener('click', e => {
+            e.preventDefault();
+            e.stopPropagation();
+            open();
+        });
+
+        modalAbout.addEventListener('click', e => {
+            e.stopPropagation();
+        });
+
         document.addEventListener('click', closeModal);
         closeBtn.addEventListener('click', closeModal);
+
+        document.addEventListener('click', close);
+        closeBtnAbout.addEventListener('click', close);
     }
 };
